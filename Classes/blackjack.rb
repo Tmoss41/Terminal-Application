b097e2d7f08f2f3a385e36702c1ff94c0db7b639
Timothy Moss <tim@Timothys-MacBook-Pro.local>
2021-04-11 15:18:03 +1000

    Missed a file when doing Initial Load Error Handling fixes

--- Classes/blackjack.rb
+++ Classes/blackjack.rb
@@ -1,4 +1,11 @@
-require_relative 'games'
+begin
+    require_relative 'games'
+rescue LoadError
+    puts "There is a Class or Module File Missing, Please Refer To Help Documentation to find a list and file structure of the
+    Application, and pull application from GIT Repo to repair"
+    exit(false)
+end
+
 class Blackjack < Games
     @tally = 0
     def initialize(balance, name)
--- modules/test_login.rb
+++
@@ -1,7 +0,0 @@
-require 'json'
-
-test_user = {user: ["Password1", 500]}
-File.open("../users.json", "a+") do |f|
-    f.puts(test_user.to_json)
-end
-
